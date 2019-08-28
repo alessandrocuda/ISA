f32a70db3d20e3e085a7ba99f7e93ef0537a1589
Alessandro <alessandro@cudazzo.com>
2019-08-29 01:12:33 +0200

    added other response

--- tr_rules.pl
+++ tr_rules.pl
@@ -29,18 +29,37 @@ response([you_are,Y], [action(Z),action(Z1),action(Z2)]):-
 
 response(Z, [action('So Long, and Thanks for All the Fish!')]):- check_quit(Z), !.
 
-response([suggest, movie], [action(Z), action(Z1), action(Z2)]):-  
+response([suggest, movie], [action(Z), action(Z1), action(Z2), action(Z3), action(Z4), action(Z5), action(Z6), action(Z7), action(Z8), action(Z9), action(Z10)]):-  
                                         random(1000, 1099, ID), movie_title(ID,T),
-                                        format(atom(Z), 'Maybe this one? ~w ', [T]),
-                                        format(atom(Z1), 'This? ~w ', [T]),
-                                        format(atom(Z2), '~w is a very nice movie! you should watch it', [T]).
+                                        format(atom(Z), 'Maybe this one? "~w"!', [T]),
+                                        format(atom(Z1), 'You might like this movie! "~w" ', [T]),
+                                        format(atom(Z2), '"~w" is a very nice movie! you should watch it', [T]),
+                                        format(atom(Z3), '"~w" is awsome! Everyone has already seen this movie!', [T]),
+                                        format(atom(Z4), 'Maybe this one? ~w You might like this movie!', [T]),
+                                        format(atom(Z5), 'A super movie is definitely "~w"! you should watch it !', [T]),
+                                        format(atom(Z6), 'Oh, let me think ... maybe "~w"?', [T]),
+                                        format(atom(Z7), 'One...two...three...maybe "~w" could be your movie! ', [T]),
+                                        format(atom(Z8), 'Of course! I\'m here for you! here\'s a movie: "~w" ', [T]),
+                                        format(atom(Z9), 'I\'m your ultimate expert, here\'s a movie: "~w" ', [T]),
+                                        format(atom(Z10), 'mmm well, it\'s hard to decide but "~w" is a good movie to watch!', [T]).
 
-response([suggest, movies], [action(Z)]):-  
+response([suggest, movies], [action(Z), action(Z1), action(Z2), action(Z3), action(Z4), action(Z5), action(Z6), action(Z7), action(Z8), action(Z9), action(Z10)]):-  
                                         random(1000, 1099, ID), movie_title(ID,T), movie_genre(ID,G),
                                         random(1000, 1099, ID1), movie_title(ID1,T1), movie_genre(ID1,G1),
                                         random(1000, 1099, ID2), movie_title(ID2,T2), movie_genre(ID2,G2),
                                         ID \== ID1, ID1 \== ID2, ID \== ID2,
-                                        format(atom(Z), 'Maybe as a ~w you could watch ~w,\nor as ~w: ~w, or maybe as ~w: ~w ', [G, T, G1, T1, G2, T2]).
+                                        format(atom(Z), 'Maybe i could recommend 3 films by genre,\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z1), 'Mmm well, it\'s hard to decide but i could suggest you 3 films by genre,\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z2), 'One...two...three...movies could be the correct answer,\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z3), 'Let me check in the magic ball...here 3 films by genre for you!\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z4), 'someone once told me that these 3 films must be seen absolutely!\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z5), 'I asked my faithful assistant for help and he says that\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z6), 'Oh wait i\'m the one! Sure i can help you!\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z7), 'Harry Potter\'s magic hat says\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z8), 'This three movies are aswsome!\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z9), 'Everyone has already seen these movies!\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+                                        format(atom(Z10), 'You might like these three movies!,\nas a ~w you could watch "~w",\nor "~w" as ~w,\nor maybe "~w" as ~w ', [G, T, T1, G1, T2, G2]),
+
 
 response([suggest, tv, show], [action(Z), action(Z1), action(Z2)]):-  
                                         random(2000, 2059, ID), tvshow_title(ID,T),
