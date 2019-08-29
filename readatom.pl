3c04215b9044d3ab36730c87d563928596a794d4
Alessandro <alessandro@cudazzo.com>
2019-08-30 00:03:01 +0200

    isa v1.0.0

--- readatom.pl
+++ readatom.pl
@@ -12,49 +12,49 @@
 %  list of atomic terms: [this,is,an,example].
 
 read_atomics(Atomics) :-
-   read_char(FirstC,FirstT),
-   complete_line(FirstC,FirstT,Atomics).
+   read_char(FirstC, FirstT),
+   complete_line(FirstC, FirstT, Atomics).
 
 
 % read_char(-Char,-Type)
 %  Reads a character and runs it through char_type/1.
 
-read_char(Char,Type) :-
+read_char(Char, Type) :-
    get0(C),
-   char_type(C,Type,Char).
+   char_type(C, Type, Char).
 
 
 % complete_line(+FirstC,+FirstT,-Atomics)
 %  Given FirstC (the first character) and FirstT (its type), reads
 %  and tokenizes the rest of the line into atoms and numbers.
 
-complete_line(_,end,[]) :- !.                  % stop at end
+complete_line(_, end, []) :- !.                  % stop at end
 
-complete_line(_,blank,Atomics) :-              % skip blanks
+complete_line(_, blank, Atomics) :-              % skip blanks
    !,
    read_atomics(Atomics).
 
-complete_line(FirstC,special,[A|Atomics]) :-   % special char
+complete_line(FirstC, special, [A|Atomics]) :-   % special char
    !,
-   name(A,[FirstC]),
+   name(A, [FirstC]),
    read_atomics(Atomics).
 
-complete_line(FirstC,alpha,[A|Atomics]) :-     % begin word
-   complete_word(FirstC,alpha,Word,NextC,NextT),
-   name(A,Word),  % may not handle numbers correctly - see text
-   complete_line(NextC,NextT,Atomics).
+complete_line(FirstC, alpha,[A|Atomics]) :-     % begin word
+   complete_word(FirstC, alpha, Word, NextC, NextT),
+   name(A, Word),  % may not handle numbers correctly - see text
+   complete_line(NextC, NextT, Atomics).
 
 
 % complete_word(+FirstC,+FirstT,-List,-FollC,-FollT)
 %  Given FirstC (the first character) and FirstT (its type),
 %  reads the rest of a word, putting its characters into List.
 
-complete_word(FirstC,alpha,[FirstC|List],FollC,FollT) :-
+complete_word(FirstC, alpha, [FirstC|List], FollC, FollT) :-
    !,
-   read_char(NextC,NextT),
-   complete_word(NextC,NextT,List,FollC,FollT).
+   read_char(NextC, NextT),
+   complete_word(NextC, NextT, List, FollC, FollT).
 
-complete_word(FirstC,FirstT,[],FirstC,FirstT).
+complete_word(FirstC, FirstT, [], FirstC, FirstT).
    % where FirstT is not alpha
 
 
@@ -63,26 +63,26 @@ complete_word(FirstC,FirstT,[],FirstC,FirstT).
 %  'end' (of line/file), 'blank', 'alpha'(numeric), or 'special',
 %  and changes it to a potentially different character (NewCode).
 
-char_type(10,end,10) :- !.         % UNIX end of line mark
-char_type(13,end,13) :- !.         % DOS end of line mark
-char_type(-1,end,-1) :- !.         % get0 end of file code
+char_type(10, end, 10) :- !.         % UNIX end of line mark
+char_type(13, end, 13) :- !.         % DOS end of line mark
+char_type(-1, end, -1) :- !.         % get0 end of file code
 
-char_type(Code,blank,32) :-        % blanks, other ctrl codes
+char_type(Code, blank, 32) :-        % blanks, other ctrl codes
   Code =< 32,
   !.
 
-char_type(Code,alpha,Code) :-      % digits
+char_type(Code, alpha, Code) :-      % digits
   48 =< Code, Code =< 57,
   !.
 
-char_type(Code,alpha,Code) :-      % lower-case letters
+char_type(Code, alpha, Code) :-      % lower-case letters
 
   97 =< Code, Code =< 122,
   !.
 
-char_type(Code,alpha,NewCode) :-   % upper-case letters
+char_type(Code, alpha, NewCode) :-   % upper-case letters
   65 =< Code, Code =< 90,
   !,
   NewCode is Code + 32.            %  (translate to lower case)
 
-char_type(Code,special,Code).      % all others
+char_type(Code, special, Code).      % all others
